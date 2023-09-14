63c20fc50aed4bfe16341af377f297a48046cf84
Abhishek Tiwari <97331593+iamabhish23@users.noreply.github.com>
2023-09-14 13:49:36 +0530

    Update CodeTriangularStarPattern.java

--- Introduction to Java/Patterns 1/CodeTriangularStarPattern.java
+++ Introduction to Java/Patterns 1/CodeTriangularStarPattern.java
@@ -33,32 +33,20 @@ Sample Output 2:
 
 
 import java.util.Scanner;
-public class Solution {
-
-     public static void printStar(int n)
-  {
-    int i,j;
-    for(i=0; i<n; i++)
-    {
-      for(j=0; j<=i; j++)
-      {
-        System.out.print("*");
-      }
-      System.out.println();
-    }
-  }
-
+public class Solution
+{
 	public static void main(String[] args) {
-		
-		/* Your class should be named Solution.
-	 	* Read input as specified in the question.
-	 	* Print output as specified in the question.
-		*/
-
-		Scanner s=new Scanner(System.in);
-   int n=s.nextInt();
-   printStar(n);
-   s.close();
+	    Scanner s = new Scanner(System.in);
+	    int n = s.nextInt();
+	    int i=1;
+	    while(i<=n){
+	        int j = 1;
+	        while(j<=i){
+	            System.out.print("*");
+	            j++;
+	        }
+	        System.out.println();
+	        i++;
+	    }    		
 	}
-
 }
